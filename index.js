92b29f783ea6e081420c533945ea0d3e2b95d913
RyanMcd29 <117286428+RyanMcd29@users.noreply.github.com>
2023-01-28 19:11:40 +0800

    create manager class

--- index.js
+++ index.js
@@ -1,6 +1,69 @@
-const Employee = require('./lib/employee')
+// const Employee = require('./lib/employee')
+const Manager = require('./lib/manager')
+const Engineer = require('./lib/engineer')
+const Intern = require('./lib/intern')
 const inquirer =  require('inquirer')
 const fs = require('fs')
+
+function getEmployeeRoleDetails(answers) {
+    console.log(answers)
+    switch(answers.employeeRole) {
+        
+
+        case 'Manager':
+            inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    message: "What is this manager's office number?",
+                    name: 'managerOffice',
+                }
+            ])
+            .then((managerAnswer)=>{
+                answers.push(managerAnswer)
+                console.log(answers)
+            })
+            
+        break
+    
+    }
+}
+
+function createManager() {
+    inquirer
+    .prompt([
+        {
+            // Get employee name
+            type: 'input',
+            message: "What is the Manager's name?",
+            name: 'name',
+        },
+        {
+            // Get employee ID
+            type: 'input',
+            message: "What is the their ID?",
+            name: 'ID',
+        },
+        {
+            // Get employee email
+            type: 'input',
+            message: "What is their email?",
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: "What is their office number?",
+            name: 'office',
+        }
+
+       
+    ])
+    .then((answers)=>{
+        const manager = new Manager(answers.name, answers.ID, answers.email, 'Manager', answers.office)
+        console.log(manager)
+    })
+}
+
 
 function createNewEmployee() {
     inquirer
@@ -25,18 +88,24 @@ function createNewEmployee() {
         },
         {
             // Get employee role
-            type: 'input',
+            type: 'rawlist',
             message: "What is the employee's role?",
             name: 'employeeRole',
+            choices: [ 'Manager', 'Engineer', 'Intern' ],
         }
        
     ])
     .then((answers)=>{
-     // Write response to file  
-        employeeName = answers.employeeName;
-        const employeeName = new Employee(answers.employeeName, answers.employeeID, answers.employeeEmail, answers.employeeRole)
+      getEmployeeRoleDetails(answers)
+    // Write response to employee class 
+
+    // answers.employeeName = new Employee(answers.employeeName, answers.employeeID, answers.employeeEmail, answers.employeeRole)
     }
+
+    
+
     )
+  
 }
 
-createNewEmployee()
+createManager()
